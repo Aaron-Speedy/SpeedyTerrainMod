78ee4fc80419a982b1235991e0062f74bc22f781
Aaron-Speedy <aaron@lspeedy.org>
2025-06-02 12:39:25 -0500

    Get rid of biome registry in OTGBiomeProvider

--- platforms/paper/src/main/java/com/pg85/otg/paper/biome/OTGBiomeProvider.java
+++ platforms/paper/src/main/java/com/pg85/otg/paper/biome/OTGBiomeProvider.java
@@ -1,6 +1,7 @@
 package com.pg85.otg.paper.biome;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.Lifecycle;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.pg85.otg.core.OTG;
@@ -18,32 +19,43 @@ import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.CraftServer;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class OTGBiomeProvider extends BiomeSource implements ILayerSource {
-    public static final Codec<OTGBiomeProvider> CODEC = RecordCodecBuilder.create(
-            (instance) -> instance.group(
-                    Codec.STRING.fieldOf("preset_name").stable().forGetter((provider) -> provider.presetFolderName),
-                    Codec.LONG.fieldOf("seed").stable().forGetter((provider) -> provider.seed),
-                    Codec.BOOL.optionalFieldOf("legacy_biome_init_layer", Boolean.FALSE, Lifecycle.stable()).forGetter((provider) -> provider.legacyBiomeInitLayer),
-                    Codec.BOOL.fieldOf("large_biomes").orElse(false).stable().forGetter((provider) -> provider.largeBiomes),
-                    RegistryOps.retrieveRegistry(Registries.BIOME).forGetter((provider) -> provider.registry)
-            ).apply(instance, instance.stable(OTGBiomeProvider::new))
-    );
+    private static final RegistryAccess registryAccess = ((CraftServer) Bukkit.getServer()).getServer().registryAccess();
+
+    public final String presetFolderName;
     private final long seed;
     private final boolean legacyBiomeInitLayer;
     private final boolean largeBiomes;
-    private final Registry<Biome> registry;
+    // private final Registry<Biome> registry;
+    // private final HolderSet<Biome> biomes;
+
     private final ThreadLocal<CachingLayerSampler> layer;
     private final Int2ObjectMap<ResourceKey<Biome>> keyLookup;
-    private final String presetFolderName;
+
+    public static final Codec<OTGBiomeProvider> CODEC = RecordCodecBuilder.create(
+        instance -> instance.group(
+            Codec.STRING.fieldOf("preset_name").stable().forGetter(x -> x.presetFolderName),
+            Codec.LONG.fieldOf("seed").stable().forGetter(x -> x.seed),
+            Codec.BOOL.optionalFieldOf("legacy_biome_init_layer", Boolean.FALSE, Lifecycle.stable()).forGetter(x -> x.legacyBiomeInitLayer),
+            Codec.BOOL.fieldOf("large_biomes").orElse(false).stable().forGetter(x -> x.largeBiomes)
+            // Registry.byNameCodec().fieldOf("registry").forGetter(x -> x.registry)
+            // registryAccess.lookupOrThrow(Registries.BIOME).byNameCodec().fieldOf("registry").forGetter(x -> x.registry)
+            // Biome.LIST_CODEC.fieldOf("biomes").forGetter(x -> x.biomes)
+            // RegistryLookupCodec.create(Registry.BIOME_REGISTRY).forGetter(x -> x.registry)
+        ).apply(instance, instance.stable(OTGBiomeProvider::new))
+    );
 
     public OTGBiomeProvider(String presetFolderName, long seed, boolean legacyBiomeInitLayer, boolean largeBiomes, Registry<Biome> registry) {
         super(getAllBiomesByPreset(presetFolderName, registry));
@@ -71,25 +83,27 @@ public class OTGBiomeProvider extends BiomeSource implements ILayerSource {
         }
     }
 
-    private static Stream<Holder<Biome>> getAllBiomesByPreset(String presetFolderName, Registry<Biome> registry) {
-        List<ResourceKey<Biome>> biomesForPreset = ((PaperPresetLoader) OTG.getEngine().getPresetLoader()).getBiomeRegistryKeys(presetFolderName);
-        if (biomesForPreset == null) {
-            ((PaperPresetLoader) OTG.getEngine().getPresetLoader()).getBiomeRegistryKeys(OTG.getEngine().getPresetLoader().getDefaultPresetFolderName());
-        }
-        if (biomesForPreset == null) {
-            biomesForPreset = new ArrayList<>();
-        }
-        return biomesForPreset.stream().map(p -> Holder.direct(registry.getOrThrow(p)));
-    }
+    // private static Stream<Holder<Biome>> getAllBiomesByPreset(String presetFolderName) {
+    //     List<ResourceKey<Biome>> biomesForPreset = ((PaperPresetLoader) OTG.getEngine().getPresetLoader()).getBiomeRegistryKeys(presetFolderName);
+    //     if (biomesForPreset == null) {
+    //         ((PaperPresetLoader) OTG.getEngine().getPresetLoader()).getBiomeRegistryKeys(OTG.getEngine().getPresetLoader().getDefaultPresetFolderName());
+    //     }
+    //     if (biomesForPreset == null) {
+    //         biomesForPreset = new ArrayList<>();
+    //     }
+    //     return biomesForPreset.stream().map(p -> Holder.direct(registry.getOrThrow(p)));
+    // }
 
-    protected Codec<? extends BiomeSource> codec() {
+    // TODO: implement collectPossibleBiomes
+
+    protected MapCodec<? extends BiomeSource> codec() {
         return CODEC;
     }
 
     // TODO: This is only used by MC internally, OTG fetches all biomes via CachedBiomeProvider.
     // Could make this use the cache too?
     @Override
-    public Holder<Biome> getNoiseBiome(int biomeX, int biomeY, int biomeZ, Climate.Sampler p_186738_) {
+    public Holder<Biome> getNoiseBiome(int biomeX, int biomeY, int biomeZ, Climate.Sampler sampler) {
         return Holder.direct(registry.get(keyLookup.get(this.layer.get().sample(biomeX, biomeZ))));
     }
 
