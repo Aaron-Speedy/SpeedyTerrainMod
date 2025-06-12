43f547754d9defb46bb9b4fed72aabc85a9ac8cb
Aaron-Speedy <aaron@lspeedy.org>
2025-06-12 18:46:00 -0500

    Update MobSpawnGroupHelper.java

--- platforms/paper/src/main/java/com/pg85/otg/paper/util/MobSpawnGroupHelper.java
+++ platforms/paper/src/main/java/com/pg85/otg/paper/util/MobSpawnGroupHelper.java
@@ -1,6 +1,8 @@
 package com.pg85.otg.paper.util;
 
 import com.pg85.otg.util.biome.WeightedMobSpawnGroup;
+
+import net.minecraft.util.random.Weighted;
 import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
@@ -13,11 +15,12 @@ public class MobSpawnGroupHelper {
     public static List<WeightedMobSpawnGroup> getListFromMinecraftBiome(Biome biome, MobCategory type) {
         WeightedList<SpawnerData> mobList = biome.getMobSettings().getMobs(type);
         List<WeightedMobSpawnGroup> result = new ArrayList<WeightedMobSpawnGroup>();
-        for (SpawnerData spawner : mobList.unwrap()) {
+        for (Weighted<SpawnerData> wSpawner : mobList.unwrap()) {
             // Removing "entities/" since the key returned is "minecraft:entities/chicken" for vanilla biomes/mobs.
             // in 1.17, this is now "minecraft:entity.minecraft.chicken"
             // TODO: Make sure this works for all mobs.
-            WeightedMobSpawnGroup wMSG = new WeightedMobSpawnGroup(spawner.type.getDescriptionId().replace("entity.minecraft.", ""), spawner.getWeight().asInt(), spawner.minCount, spawner.maxCount);
+            SpawnerData spawner = wSpawner.value();
+            WeightedMobSpawnGroup wMSG = new WeightedMobSpawnGroup(spawner.type().getDescriptionId().replace("entity.minecraft.", ""), wSpawner.weight().asInt(), spawner.minCount(), spawner.maxCount);
             result.add(wMSG);
         }
         return result;
